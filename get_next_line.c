3e6970249799cb9277d103ee28510c3547120ab8
Alba Castano <acastano@student.hive.fi>
2022-01-18 20:48:08 +0200

    get_next_line.c : created temp string to hold all read characters

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: acastano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 15:02:58 by acastano          #+#    #+#             */
-/*   Updated: 2022/01/18 19:08:53 by acastano         ###   ########.fr       */
+/*   Updated: 2022/01/18 20:42:36 by acastano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,15 +24,35 @@
 int	get_next_line(const int fd, char **line)
 {
 	char	*buf;
+	char	*temp;
 	size_t	chars_read;
-//	size_t	**fd_position;
+//	static int	fd_position;
 
 	if (fd < 0 || line == NULL)//if no file or no place to return possible read line
 		return (-1);
 	buf = ft_strnew(BUFF_SIZE + 1);
 	if (!buf)
 		return (-1);
+//	fd_position = fd;
+//	chars_read = read(fd_position, buf, BUFF_SIZE);
 	chars_read = read(fd, buf, BUFF_SIZE);
+	temp = buf;
+	while (read(fd, buf, BUFF_SIZE))
+	{
+		temp = ft_strjoin(temp + buf);	
+		if (ft_strchr(temp, "\n"))
+			break;
+	}
+//\n EOF
 	*line = buf;
 	return (chars_read);
 }
+
+/*
+{
+
+
+	{
+		
+	}
+	}*/
