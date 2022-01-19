c6da05115908fd001144d8566ba4830883281c1e
Alba Castano <acastano@student.hive.fi>
2022-01-19 19:18:38 +0200

    get_next_line.c : Added static variable (array of char *) to track fd's reading positions. Changed return values to -1(fail), 0(didn't read) and 1(read) instead of number of bytes read.

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: acastano <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 15:02:58 by acastano          #+#    #+#             */
-/*   Updated: 2022/01/18 20:42:36 by acastano         ###   ########.fr       */
+/*   Updated: 2022/01/19 19:11:02 by acastano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,45 +14,49 @@
 #include "./libft/libft.h"
 #include <unistd.h>//read
 #include <stdio.h>//printf
+#include <stdlib.h>//free
 //# define BUFF_SIZE 42 defined in get_next_line.h
-
-/*size_t	buff_size()
-{
-
-}*/
+# define FD_SIZE 4096
 
 int	get_next_line(const int fd, char **line)
 {
 	char	*buf;
 	char	*temp;
-	size_t	chars_read;
-//	static int	fd_position;
+	size_t	bytes_read;
+	int	newline;
+	static char	*fd_tracker[FD_SIZE];
 
+	newline = 1;
 	if (fd < 0 || line == NULL)//if no file or no place to return possible read line
 		return (-1);
+
 	buf = ft_strnew(BUFF_SIZE + 1);
 	if (!buf)
 		return (-1);
-//	fd_position = fd;
-//	chars_read = read(fd_position, buf, BUFF_SIZE);
-	chars_read = read(fd, buf, BUFF_SIZE);
-	temp = buf;
+	bytes_read = read(fd, buf, BUFF_SIZE);
+	temp = ft_strdup(buf);
+	if (!temp)
+	{
+		free(buf);
+		return(-1);
+	}
 	while (read(fd, buf, BUFF_SIZE))
 	{
-		temp = ft_strjoin(temp + buf);	
-		if (ft_strchr(temp, "\n"))
+		if (ft_strchr(temp, '\n'))
+		{
+			newline = 0;
 			break;
-	}
-//\n EOF
-	*line = buf;
-	return (chars_read);
+		}
+		temp = ft_strjoin(temp, buf);
+		bytes_read = ft_strlen(temp);
+//		if (ft_strchr(temp, '\n'))
+//			break;
 }
-
-/*
-{
-
-
+//\n EOF
+	if (newline)//get string ending before the \n
 	{
-		
+		ft_memccpy(fd_tracker[fd], temp, '\n', bytes_read);
 	}
-	}*/
+	*line = temp;
+	return ((bytes_read == 0) ? 0 : 1);
+}
